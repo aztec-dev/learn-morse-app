b05ffea2ea4301e2247f9d40a8622ec9b897fa89
aztec-dev <aztech.dev1@gmail.com>
2024-11-26 21:06:12 +1000

    Initial commit

---
+++ data/data.ts
@@ -0,0 +1,13 @@
+/*
+This file contains an object that holds data.
+The data contained inside this object represents the letters and 
+the Morse code associated with the letter respectively. 
+*/
+
+const data = [
+    {id:1, letter: 'A', code: '../../assets/images/a_morse_code.png'},
+    {id:2, letter: 'B', code: '../../assets/images/b_morse_code.png'},
+    {id: 3, letter: 'J', code: '../../assets/images/j_morse_code.png'}
+];
+
+export default data;
