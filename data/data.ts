bb05be6556a3ff70fdbb7ded94b0fa205188eb11
aztec-dev <aztech.dev1@gmail.com>
2024-11-29 18:19:15 +1000

    [#1]FEAT: Added full alphabet and morse code

--- data/data.ts
+++ data/data.ts
@@ -5,9 +5,32 @@ the Morse code associated with the letter respectively.
 */
 
 const data = [
-    {id:1, letter: 'A', code: '../../assets/images/a_morse_code.png'},
-    {id:2, letter: 'B', code: '../../assets/images/b_morse_code.png'},
-    {id: 3, letter: 'J', code: '../../assets/images/j_morse_code.png'}
+    {id:1, letter: 'A', code: require('../assets/images/codes/b_morse_code.png')},
+    {id:2, letter: 'B', code: require('../assets/images/codes/b_morse_code.png')},
+    {id:3, letter: 'C', code: require('../assets/images/codes/c_morse_code.png')},
+    {id:4, letter: 'D', code: require('../assets/images/codes/d_morse_code.png')},
+    {id:5, letter: 'E', code: require('../assets/images/codes/e_morse_code.png')},
+    {id:6, letter: 'F', code: require('../assets/images/codes/f_morse_code.png')},
+    {id:7, letter: 'G', code: require('../assets/images/codes/g_morse_code.png')},
+    {id:8, letter: 'H', code: require('../assets/images/codes/h_morse_code.png')},
+    {id:9, letter: 'I', code: require('../assets/images/codes/i_morse_code.png')},
+    {id:10, letter: 'J', code: require('../assets/images/codes/j_morse_code.png')},
+    {id:11, letter: 'K', code: require('../assets/images/codes/k_morse_code.png')},
+    {id:12, letter: 'L', code: require('../assets/images/codes/l_morse_code.png')},
+    {id:13, letter: 'M', code: require('../assets/images/codes/m_morse_code.png')},
+    {id:14, letter: 'N', code: require('../assets/images/codes/n_morse_code.png')},
+    {id:15, letter: 'O', code: require('../assets/images/codes/o_morse_code.png')},
+    {id:16, letter: 'P', code: require('../assets/images/codes/p_morse_code.png')},
+    {id:17, letter: 'Q', code: require('../assets/images/codes/q_morse_code.png')},
+    {id:18, letter: 'R', code: require('../assets/images/codes/r_morse_code.png')},
+    {id:19, letter: 'S', code: require('../assets/images/codes/s_morse_code.png')},
+    {id:20, letter: 'T', code: require('../assets/images/codes/t_morse_code.png')},
+    {id:21, letter: 'U', code: require('../assets/images/codes/u_morse_code.png')},
+    {id:22, letter: 'V', code: require('../assets/images/codes/v_morse_code.png')},
+    {id:23, letter: 'W', code: require('../assets/images/codes/w_morse_code.png')},
+    {id:24, letter: 'X', code: require('../assets/images/codes/x_morse_code.png')},
+    {id:25, letter: 'Y', code: require('../assets/images/codes/y_morse_code.png')},
+    {id:26, letter: 'Z', code: require('../assets/images/codes/z_morse_code.png')},
 ];
 
 export default data;
